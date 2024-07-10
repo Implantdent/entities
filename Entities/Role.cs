3fbf4780655f09981e640487757c85104910b142
Leandro Baena Torres <leandrobaena@paradigma.com.co>
2024-07-10 09:50:35 -0500

    Adición de entidades para los roles

---
+++ Entities/Role.cs
@@ -0,0 +1,27 @@
+﻿namespace Entities
+{
+    /// <summary>
+    /// Rol de la aplicación
+    /// </summary>
+    public class Role
+    {
+        /// <summary>
+        /// Identificador del rol
+        /// </summary>
+        public short Id { get; set; }
+
+        /// <summary>
+        /// Nombre del rol
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Inicializa el objeto con valores por defecto
+        /// </summary>
+        public Role()
+        {
+            Id = 0;
+            Name = string.Empty;
+        }
+    }
+}
